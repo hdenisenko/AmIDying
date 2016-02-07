7f31d5c6ef7be3ba071ea6e6d9fe0502aea536ad
wflotte <william_flotte@brown.edu>
2016-02-07 01:19:16 -0500

    this should be up to date

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('sampleindex.html',my_string="HIIIIII",my_list=[0,1,2,3,4,5])
+    return render_template('sampleindex.html',my_string="HIIIIII",my_color="round yellow",my_list=[ [0,1,2],[3,4,5],[6,7]])
 
- 
+
 if __name__ == '__main__':
     app.run(debug=True)

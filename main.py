19e8f03988efab7d306b8a148f695c8d5b326181
KunhaoZhou <z.kunhao@gmail.com>
2016-02-07 02:56:29 -0500

    tired but can

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-
 from flask import Flask, render_template
+from jinja2 import Template
+from lxml import html
+import requests
 app = Flask(__name__)
-
-
 @app.route("/")
+
+
 def template_test():
-    return render_template('sampleindex.html',my_string="HIIIIII",my_color="round yellow",my_list=[ [0,1,2],[3,4,5],[6,7]])
+    root = 'http://symptomchecker.webmd.com/'
+    page = requests.get(root + 'symptoms-a-z')
+    tree = html.fromstring(page.content)
+    links1 = tree.xpath('//a[@onclick="return sl(this, \'\', \'sc-symindex-az_1\');"]/@href')
+    symptoms1 = tree.xpath('//a[@onclick="return sl(this, \'\', \'sc-symindex-az_1\');"]/text()')
+    mylist = []
+    for i in range(len(symptoms1)//11):
+        temp = []
+        for j in range(11):
+            temp.append(symptoms1[i+j])
+        mylist.append(temp)
+
+    return render_template('sampleindex.html', my_string="Wheeeee!", my_list = mylist)
 
 
 if __name__ == '__main__':

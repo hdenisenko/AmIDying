0bebbb383f2d11cb896bbaf128941943fbb700af
Polina Tamarina <polina@sproutel.com>
2016-02-06 21:00:28 -0500

    front end changes

---
+++ frontend /js/index.js
@@ -0,0 +1,12 @@
+
+var json = '"symptoms":[
+    {"symptom":"death"}, 
+    {"symptom":"bloating"}, 
+    {"symptom":"sneezing"},
+    {"symptom":"arm rot"},
+    {"symptom":"fever"},
+    {"symptom":"fatigue"},
+    {"symptom":"leg rot"}
+]'
+
+//parse 

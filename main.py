58def89c5cdac6c102122d05a0715b67c1c7e9b5
wflotte <william_flotte@brown.edu>
2016-02-07 11:07:26 -0500

    maping works

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 button_label = []
 with open("test.csv",'rU') as f: button_label.extend(f.read().split(','))
 
+
+
 color_list = []
 for i in range(len(button_label)-1):
 	temp_id = i % 3
@@ -18,8 +20,15 @@ button_list = zip(button_label, color_list)
 
 @app.route("/")
 def template_test():
-
 	return render_template('sampleindex.html', button_list=button_list)
+
+@app.route("/symptoms2/<string:symptID>")
+def symptoms2ndChoice(symptID):
+	return render_template('sampleindex2.html',symptID=str(symptID),button_list=button_list)
+
+@app.route("/symptoms3/<string:symptID>/<string:symptID2>")
+def symptoms3ndChoice(symptID,symptID2):
+	return render_template('sampleindex3.html', button_list=button_list)
 
  
 if __name__ == '__main__':
